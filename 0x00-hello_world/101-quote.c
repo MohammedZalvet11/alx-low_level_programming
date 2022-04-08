7b4f6044a693fae13d4df7815af44c0f1f50970e
MohammedZalvet11 <mohammedzalvet@gamil.com>
2022-04-07 17:23:58 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,6 @@
  */
 int main(void)
 {
-	writes(2, "and that piece of art is useful\" - Dors Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dors Korpar, 2015-10-19\n", 59);
 	return (1);
 }
