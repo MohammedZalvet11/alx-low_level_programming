e974394622a24fa1897b63b2aa466c1be1bfda3c
MohammedZalvet11 <mohammedzalvet@gamil.com>
2022-04-08 15:11:57 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers of base 10 starting from 0
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int number;
+	while (number <= 9)
+	{
+		printf("%d", number);
+		number++;
+	}
+	printf("\n");
+	return (0);
+}
