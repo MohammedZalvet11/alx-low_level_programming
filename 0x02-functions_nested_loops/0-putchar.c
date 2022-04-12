4af1c54ab342cb229d982cec656459caae7e492b
MohammedZalvet11 <mohammedzalvet@gamil.com>
2022-04-12 06:10:15 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Print the word "_putchar"
+ *
+ * Return: Always o (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
