d25908a60bd4faf7fee9d51710638b1384fbe661
MohammedZalvet11 <mohammedzalvet@gamil.com>
2022-04-20 08:43:32 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int(int *a, int *b)
+ * @a: pointer a
+ * @b: pointer b
+ * Return void
+ */
+void swap_int(int *a, int *b)
+{
+	*a += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+
+}
