c9da8c933353c6d13767aaf4a1765fa7b3913203
MohammedZalvet11 <mohammedzalvet@gamil.com>
2022-04-20 07:49:39 -0700

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update the value it point
+ * @n: pointer of the direction at the variable n
+ * Return: 0 (success)
+ */
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
