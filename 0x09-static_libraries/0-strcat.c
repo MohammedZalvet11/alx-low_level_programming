d3582e3569171ee5c5f45ef95d1bead00feb8f39
MohammedZalvet11 <mohammedzalvet@gmail.com>
2022-05-04 16:14:01 -0700

    First commit

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * *_strcat - function commute strings
+ * @dest: pointer to a char
+ * @src: pointer to a char
+ * Return: return value of dest
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int a;
+	int b;
+
+	a = 0;
+	b = 0;
+
+	while (dest[a] != '\0')
+	{
+		a++;
+	}
+
+	while (src[b] != '\0')
+	{
+		dest[a] = src[b];
+		b++;
+		a++;
+	}
+
+	dest[a] = '\0';
+	return (dest);
+}
